056672741ad7aee199116f41bb0d9999ec553402
momskyh <37992953+momskyh@users.noreply.github.com>
2018-05-09 11:59:56 +0700

    Update push.php

--- php/push.php
+++ php/push.php
@@ -4,12 +4,13 @@
 
 include ('line-bot-api/php/line-bot.php');
 
-$channelSecret = '551ec4feee0.....43cff0';
-$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'h6qzlIg7k7SdqLjiq4CCa+AuVQpCGoPF+cg78G+HlBrBH1v6u2D/SP6slIAsydQAqqUoFR3Z5XVRlh4CnTULSDEkZmeUwXHUhTcu2N+XzgUTQVWW4oOrlMQnoIORLgV6H52Ctn3cEuIOrPUJbXWDTQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '81723630c588835f06fbf9918436be6a';
+$idPush = 'Ud5680fffd4957a5bc2af997beabc72ba'
+	
 $bot = new BOT_API($channelSecret, $access_token);
 	
-$bot->sendMessageNew('[Your userId / User Id]', 'Hello World !!');
+$bot->sendMessageNew($idPush, 'Hello World !!');
 
 if ($bot->isSuccess()) {
 	echo 'Succeeded!';
